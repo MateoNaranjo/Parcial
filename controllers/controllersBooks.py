1dc4ea6be2a54c4f4b9402d7f2bf31b9a60652dc
MateoNaranjo <mateo.naranjo.artunduaga@gmail.com>
2025-09-04 19:25:33 -0500

    Main: Adaptacion a libros

--- controllers/controllersBooks.py
+++ controllers/controllersBooks.py
@@ -1,13 +1,13 @@
 from flask import Blueprint
 
-books_bp = Blueprint('band_bp', __name__)
+books_bp = Blueprint('books_bp', __name__)
 
-@books_bp.route('/bands', methods=['GET'])
+@books_bp.route('/books', methods=['GET'])
 
-@books_bp.route('/bands/<int:band_id>', methods=['GET'])
+@books_bp.route('/books/<int:band_id>', methods=['GET'])
 
-@books_bp.route('/bands', methods=['POST'])
+@books_bp.route('/books', methods=['POST'])
 
-@books_bp.route('/bands/<int:band_id>', methods=['PUT'])
+@books_bp.route('/books/<int:band_id>', methods=['PUT'])
 
-@books_bp.route('/bands/<int:band_id>', methods=['DELETE'])
+@books_bp.route('/books/<int:band_id>', methods=['DELETE'])
